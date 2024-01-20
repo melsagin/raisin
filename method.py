38e2fcc5f54763a8a5f1f3125bf06847f43efb41
melsagin <melsasagin@gmail.com>
2024-01-20 16:38:26 +0300

    add

--- method.py
+++ method.py
@@ -1,4 +1,6 @@
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
@@ -42,3 +44,15 @@ def encode_class(df, class_column="Class"):
     df_copy.drop(class_column, axis=1, inplace=True)
     df_copy[class_column] = encoded_class
     return df_copy
+
+def find_best_k(X_train, y_train, max_k=20):
+    best_k = 0
+    best_score = 0
+    for k in range(1, max_k+1):
+        knn_model = KNeighborsClassifier(n_neighbors=k)
+        scores = cross_val_score(knn_model, X_train, y_train, cv=5, scoring='accuracy')
+        mean_score = scores.mean()
+        if mean_score > best_score:
+            best_score = mean_score
+            best_k = k
+    return best_k
